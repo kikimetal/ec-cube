73a5d21f115bbf51b4c533becded432b8ca7a5f7
kikimetal <reezafratzitz2@gmail.com>
2017-09-05 17:23:26 +0900

    gulp sass 周りの設定変更 watch 追加

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,24 +2,44 @@ import gulp from "gulp"
 import sass from "gulp-sass"
 import concat from "gulp-concat"
 import autoprefixer from "gulp-autoprefixer"
+
 // import postcss from "gulp-postcss"
 // import cssnext from "postcss-cssnext"
-
 // const processors = [
 //     cssnext({browsers: ['last 2 version']})
 // ];
+// .pipe(postcss(processors))
 
-gulp.task("default", ()=>{
-    return gulp.src("./ec/html/template/default/sass/*.sass")  // .scss/.sass ファイルを取得
+const source_dir = "./ec/html/template/default/sass/*.sass"
+const output_dir = "./ec/html/template/default/css/"
+const bundle_css_name = "mystyle-bundle.css"
+
+gulp.task("css", ()=>{
+    return gulp.src(source_dir)  // .scss/.sass ファイルを取得
         .pipe(sass({
-            // outputStyle: "expanded" // コンパイルする際の CSS の書式を指定できる
+            outputStyle: "expanded" // コンパイルする際の CSS の書式を指定できる
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat(bundle_css_name))
+        .pipe(gulp.dest(output_dir));  // cssフォルダー以下に保存
+})
+
+gulp.task("css-min", ()=>{
+    return gulp.src(source_dir)  // .scss/.sass ファイルを取得
+        .pipe(sass({
             outputStyle: "compressed" // コンパイルする際の CSS の書式を指定できる
         }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        // .pipe(postcss(processors))
-        .pipe(concat("mystyle-bundle.css"))
-        .pipe(gulp.dest("./ec/html/template/default/css/"));  // cssフォルダー以下に保存
-});
+        .pipe(concat(bundle_css_name))
+        .pipe(gulp.dest(output_dir));  // cssフォルダー以下に保存
+})
+
+gulp.task("watch", ["css"], ()=>{
+    gulp.watch(source_dir, ["css"])
+})
